1ed4510206d3853beff55353032ca939d01be24e
Artemis051 <aekatsi@gmail.com>
2023-12-28 01:56:39 -0500

    revised server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,5 @@
-const cors = require('cors');
-app.use(cors());
-
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Import 'cors' once
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -10,12 +7,10 @@ const PORT = process.env.PORT || 5500;
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/api/notes', (req, res) => {
   res.json({ message: 'API endpoint for notes' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
