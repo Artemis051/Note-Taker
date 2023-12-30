5913995213526f97a8d8c5eed31a8d465143df44
Artemis051 <aekatsi@gmail.com>
2023-12-29 20:29:04 -0500

    add cors package

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const cors = require('cors');
 const express = require('express');
-const cors = require('cors'); // Import 'cors' once
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5500;
